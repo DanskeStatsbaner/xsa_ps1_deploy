2c7c72fd81746832530f0f428b4cded5bfdac309
XLAMI1707 <xlmil@dsb.dk>
2021-01-08 16:30:53 +0100

    More scripts

---
+++ Infrastructure/DBPrep.ps1
@@ -0,0 +1,35 @@
+﻿$environment = $OctopusParameters["Octopus.Environment.Name"]
+
+# exit hvis miljø er forskellig fra Produktion (prd)
+if($environment -ne "prd") { Exit }
+
+write-host "*******************************************************************"
+write-host " START DBPrep.ps1"
+write-host "*******************************************************************"
+
+$DBpw = $args[0]
+$DBuser = $OctopusParameters["dataART.DBUser[$environment]"]
+
+$workdirPath = $(pwd)
+$projectName = $OctopusParameters["Octopus.Project.Name"]
+$releaseNumber = $OctopusParameters["Octopus.Release.Number"]
+
+$XSAurl = $OctopusParameters["dataART.XSAUrl[$environment]"]
+$XSAuser = $OctopusParameters["dataART.XSAUser[$environment]"]
+$XSAspace = $OctopusParameters["dataART.XSASpace[$environment]"]
+
+$HANAHost = $OctopusParameters["dataART.Host[$environment]"]
+$HANAInstance = $OctopusParameters["dataART.Instance[$environment]"]
+$HANADatabase = $OctopusParameters["dataART.Database[$environment]"]
+
+$allLines = 'CALL "SYSTEM"."GRANT_REMOTE_SOURCE_ACCESS"(EX_MESSAGE => ?);'
+
+Set-Content -Path c:\octopus\work\testSQLoneLine.txt -Value $allLines
+
+write-host "*** Run DB prepare SQL"
+
+docker run -v c:\octopus\work:/data artifactory.azure.dsb.dk/docker/xsa_cli_deploy /bin/sh -c "hdbsql -n $HANAHost -i $HANAInstance -d $HANADatabase -u $DBuser -p $DBpw -quiet -a -I /data/testSQLoneLine.txt -o /data/testSQLoutput.txt"
+
+write-host "*******************************************************************"
+write-host " STOP DBPrep.ps1"
+write-host "*******************************************************************"
---
+++ Infrastructure/predeploy.ps1
@@ -0,0 +1,84 @@
+﻿$environment = $OctopusParameters["Octopus.Environment.Name"]
+
+# exit hvis miljø er Sandbox (sit)
+if($environment -eq "sit") { Exit }
+
+write-host "*******************************************************************"
+write-host " START predeploy.ps1"
+write-host "*******************************************************************"
+
+$XSAPW = $args[0]
+
+$workdirPath = $(pwd)
+$projectName = $OctopusParameters["Octopus.Project.Name"]
+$releaseNumber = $OctopusParameters["Octopus.Release.Number"]
+
+$XSAurl = $OctopusParameters["dataART.XSAUrl[$environment]"]
+$XSAuser = $OctopusParameters["dataART.XSAUser[$environment]"]
+$XSAspace = $OctopusParameters["dataART.XSASpace[$environment]"]
+
+$HANAHost = $OctopusParameters["dataART.Host[$environment]"]
+$HANAInstance = $OctopusParameters["dataART.Instance[$environment]"]
+$HANADatabase = $OctopusParameters["dataART.Database[$environment]"]
+
+write-host "*** Get MTA information for $projectName"
+
+docker run -v c:\octopus\work:/data artifactory.azure.dsb.dk/docker/xsa_cli_deploy /bin/sh -c "xs login -u $XSAuser -p $XSAPW -a $XSAurl -o orgname -s $XSAspace && xs mta $projectName > /data/$($projectName)-serviceName.txt"
+
+$File = Get-Content c:\octopus\work\$($projectName)-serviceName.txt
+
+foreach ($line in $File)
+{
+    $Arr = $line -split ' '
+    foreach ($cell in $Arr)
+    {
+        if ($cell -eq 'hdi-shared'){$serviceName = $Arr[0]}
+    }
+}
+
+$serviceKey = $($serviceName) + "-sk"
+
+write-host "*** Setup servicekey $serviceKey"
+
+docker run -v c:\octopus\work:/data artifactory.azure.dsb.dk/docker/xsa_cli_deploy /bin/sh -c "xs login -u $XSAuser -p $XSAPW -a $XSAurl -o orgname -s $XSAspace && xs create-service-key $serviceName $serviceKey && xs service-key $serviceName $serviceKey > /data/$($projectName)-serviceKey.txt"
+
+$File = Get-Content c:\octopus\work\$($projectName)-serviceKey.txt
+
+foreach ($line in $File)
+{
+    $Arr = $line -split ' '
+    foreach ($cell in $Arr)
+    {
+        if ($cell -eq '"user"')
+        {
+            $userArr = $Arr[4] -split '"'
+        }
+        if ($cell -eq '"password"')
+        {
+            $passwordArr = $Arr[4] -split '"'
+        }
+    }
+}
+
+
+$DBuser = $userArr[1]
+$DBpw = $passwordArr[1]
+
+$File = Get-Content c:\octopus\work\testSQL.txt
+
+$allLines = [string]::join(" ",($File.Split("`n")))
+$allLines = [string]::join(" ",($allLines.Split("`r")))
+
+Set-Content -Path c:\octopus\work\testSQLoneLine.txt -Value $allLines
+
+write-host "*** Run post-deployment SQL"
+
+docker run -v c:\octopus\work:/data artifactory.azure.dsb.dk/docker/xsa_cli_deploy /bin/sh -c "hdbsql -n $HANAHost -i $HANAInstance -d $HANADatabase -u $DBuser -p $DBpw -quiet -a -I /data/testSQLoneLine.txt -o /data/testSQLoutput.txt"
+
+write-host "*** Cleanup - delete servicekey"
+
+docker run -v c:\octopus\work:/data artifactory.azure.dsb.dk/docker/xsa_cli_deploy /bin/sh -c "xs login -u $XSAuser -p $XSAPW -a $XSAurl -o orgname -s $XSAspace && xs delete-service-key $serviceName $serviceKey -f"
+
+write-host "*******************************************************************"
+write-host " STOP predeploy.ps1"
+write-host "*******************************************************************"
