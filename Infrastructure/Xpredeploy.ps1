d61b189ae9c06c038d7bba9ad110f6ef799cdd0b
Lars Millgaard(EXT) <XLMIL@DSB.DK>
2022-08-11 14:20:39 +0200

    Update Infrastructure/Xpredeploy.ps1

--- Infrastructure/Xpredeploy.ps1
+++ Infrastructure/Xpredeploy.ps1
@@ -39,7 +39,7 @@ write-host "workdirPath: " $workdirPath
 $allFiles = get-childitem "$workdirPath" -Recurse
 foreach($file in $allFiles ) { write-host "File: " $file.FullName }
 
-$workdirPath = $workdirPath.Substring(2, $workdirPath.IndexOf("/Deployment")-2)
+# $workdirPath = $workdirPath.Substring(2, $workdirPath.IndexOf("/Deployment")-2)
 
 $fullPath = "$($workdirPath)/Deployment/PreDeploy/$($environment)/*.txt"
 if (Test-Path $($fullPath)) {}
